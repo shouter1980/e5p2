ac622e78787a57218270be0cd4743b2a5226a69b
shouter1980 <shouter1980@gmail.com>
2019-06-20 10:54:46 +1000

    1st push

---
+++ src/e5p2/readExcel.java
@@ -0,0 +1,40 @@
+package e5p2;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class readExcel {
+ public String[][] getCellData(String path, String sheetName) throws InvalidFormatException, IOException {
+  FileInputStream stream = new FileInputStream(path);
+  Workbook workbook = WorkbookFactory.create(stream);
+  Sheet s = workbook.getSheet(sheetName);
+  int rowcount = s.getLastRowNum();
+  int cellcount = s.getRow(0).getLastCellNum();
+  String data[][] = new String[rowcount][cellcount];
+  for (int i = 1; i <= rowcount; i++) {
+   Row r = s.getRow(i);
+   //for (int j = 0; j < cellcount; j++) {
+   for (int j = 0; j < 2; j++) {
+    Cell c = r.getCell(j);
+    try {
+     if (c.getCellType() == CellType.STRING) {
+      data[i - 1][j] = c.getStringCellValue();
+     } else {
+      //data[i - 1][j] = String.valueOf(c.getNumericCellValue());
+    	 data[i - 1][j] = String.format ("%.0f", c.getNumericCellValue());
+     }
+    } catch (Exception e) {
+     e.printStackTrace();
+    }
+   }
+  }
+  return data;
+ }
+}
