65050481fc5c9d79a8c1cb14b90f8359cd3d7403
shouter1980 <shouter1980@gmail.com>
2019-06-20 10:58:23 +1000

    commit. No push

---
+++ src/e5p2/Screenshots.java
@@ -0,0 +1,19 @@
+package e5p2;
+
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class Screenshots {
+	  public static void takeSnapShot(WebDriver webdriver,String fileWithPath) throws Exception{
+
+	    	TakesScreenshot scrShot =((TakesScreenshot)webdriver);
+	    	File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+	        File DestFile=new File(fileWithPath);
+	        FileUtils.copyFile(SrcFile, DestFile);
+	  }
+
+
+}
