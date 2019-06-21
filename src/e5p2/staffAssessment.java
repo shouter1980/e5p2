7d7180fc8689c2cabfaf6c09e63e7e79a152e41c
shouter1980 <shouter1980@gmail.com>
2019-06-21 14:04:34 +1000

    6 courses now. Last one is AB-AV

--- src/e5p2/staffAssessment.java
+++ src/e5p2/staffAssessment.java
@@ -8,7 +8,11 @@ public class staffAssessment {
 	public static void verifyCourse(WebDriver driver, String courseName) throws Exception {
 		switch (courseName) {
 
+		
+		case "GC-CMGT":
+		case "GD-FBS":
 		case "MA-CIMGT":
+			//7 questions
 			driver.findElement(By.xpath(
 					"//*[@id=\"RequirementListSection_RDP_ThumbnailViewer_CARD\"]/div[4]/div/div[1]/div/div[4]/div[1]/div/ul/li/button[1]/span"))
 					.click();
@@ -136,8 +140,12 @@ public class staffAssessment {
 			
 			break;
 		
+			
+		
+		
 		case "BA-BUS10":
 		case "BA-CS":
+			//10 questions
 			driver.findElement(By.xpath(
 					"//*[@id=\"RequirementListSection_RDP_ThumbnailViewer_CARD\"]/div[4]/div/div[2]/div/div[4]/div[1]/div/ul/li/button[1]"))
 					.click();
@@ -309,9 +317,175 @@ public class staffAssessment {
 					Keys.TAB, Keys.TAB, Keys.TAB, Keys.TAB, Keys.TAB, Keys.TAB, "Australia", Keys.ENTER, Keys.ENTER,
 					Keys.TAB, Keys.TAB);
 			Thread.sleep(2000);
-
 			
 			break;
+			
+			
+		case "AB-AV":
+			//8 questions
+			driver.findElement(By.xpath(
+					"//*[@id=\"RequirementListSection_RDP_ThumbnailViewer_CARD\"]/div[4]/div/div[1]/div/div[4]/div[1]/div/ul/li/button[1]/span"))
+					.click();
+			Thread.sleep(3000);
+			driver.findElement(
+					By.xpath("//*[@id='RequirementSection_Requirement_VerificationStatus_Container']/div[2]/button"))
+					.click();
+			Thread.sleep(2000);
+			driver.findElement(By.xpath("//td[contains(text(),'Verified')]")).click();
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//div[@id='RequirementSection_TopActionPanel']")).click();
+			Thread.sleep(3000);
+			
+			driver.findElement(By.xpath(
+					"//*[@id=\"RequirementListSection_RDP_ThumbnailViewer_CARD\"]/div[4]/div/div[4]/div/div[4]/div[1]/div/ul/li/button[1]/span"))
+					.click();
+			Thread.sleep(3000);
+			driver.findElement(
+					By.xpath("//*[@id='RequirementSection_Requirement_VerificationStatus_Container']/div[2]/button"))
+					.click();
+			Thread.sleep(2000);
+			driver.findElement(By.xpath("//td[contains(text(),'Verified')]")).click();
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//div[@id='RequirementSection_TopActionPanel']")).click();
+			Thread.sleep(3000);
+			
+			driver.findElement(By.xpath(
+					"//*[@id=\"RequirementListSection_RDP_ThumbnailViewer_CARD\"]/div[4]/div/div[7]/div/div[4]/div[1]/div/ul/li/button[1]/span"))
+					.click();
+			Thread.sleep(3000);
+			driver.findElement(
+					By.xpath("//*[@id='RequirementSection_Requirement_VerificationStatus_Container']/div[2]/button"))
+					.click();
+			Thread.sleep(2000);
+			driver.findElement(By.xpath("//td[contains(text(),'Verified')]")).click();
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//div[@id='RequirementSection_TopActionPanel']")).click();
+			Thread.sleep(3000);
+			
+			driver.findElement(By.xpath(
+					"//*[@id=\"RequirementListSection_RDP_ThumbnailViewer_CARD\"]/div[4]/div/div[8]/div/div[4]/div[1]/div/ul/li/button[1]/span"))
+					.click();
+			Thread.sleep(3000);
+			driver.findElement(
+					By.xpath("//*[@id='RequirementSection_Requirement_VerificationStatus_Container']/div[2]/button"))
+					.click();
+			Thread.sleep(2000);
+			driver.findElement(By.xpath("//td[contains(text(),'Verified')]")).click();
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//div[@id='RequirementSection_TopActionPanel']")).click();
+			Thread.sleep(3000);
+			
+			
+			driver.findElement(By.xpath(
+					"//*[@id=\"RequirementListSection_RDP_ThumbnailViewer_CARD\"]/div[4]/div/div[2]/div/div[4]/div[1]/div/ul/li/button[1]/span"))
+					.click();
+			Thread.sleep(3000);
+			driver.findElement(
+					By.xpath("//*[@id='RequirementSection_Requirement_VerificationStatus_Container']/div[2]/button"))
+					.click();
+			Thread.sleep(2000);
+			driver.findElement(By.xpath("//td[contains(text(),'Verified')]")).click();
+			Thread.sleep(1000);
+			driver.findElement(
+					By.xpath("//*[@id='RequirementSection_Requirement_VerificationStatus_Container']/div[2]/button"))
+					.sendKeys(Keys.TAB, Keys.TAB, "No", Keys.ENTER, Keys.ENTER, Keys.TAB, Keys.TAB);
+			Thread.sleep(1000);
+			driver.findElement(
+					By.xpath("//*[@id='RequirementSection_Requirement_VerificationStatus_Container']/div[2]/button"))
+					.sendKeys(Keys.TAB, Keys.TAB, "No", Keys.ENTER, Keys.ENTER, Keys.TAB, Keys.TAB);
+			Thread.sleep(2000);
+			driver.findElement(By.xpath("//div[@id='RequirementSection_TopActionPanel']")).click();
+			Thread.sleep(3000);
+			
+			
+			driver.findElement(By.xpath(
+					"//*[@id=\"RequirementListSection_RDP_ThumbnailViewer_CARD\"]/div[4]/div/div[5]/div/div[4]/div[1]/div/ul/li/button[1]/span"))
+					.click();
+			Thread.sleep(3000);
+			driver.findElement(
+					By.xpath("//*[@id='RequirementSection_Requirement_VerificationStatus_Container']/div[2]/button"))
+					.click();
+			Thread.sleep(2000);
+			driver.findElement(By.xpath("//td[contains(text(),'Verified')]")).click();
+			Thread.sleep(1000);
+			driver.findElement(
+					By.xpath("//*[@id='RequirementSection_Requirement_VerificationStatus_Container']/div[2]/button"))
+					.sendKeys(Keys.TAB, Keys.TAB, "No", Keys.ENTER, Keys.ENTER, Keys.TAB, Keys.TAB);
+			Thread.sleep(1000);
+			driver.findElement(
+					By.xpath("//*[@id='RequirementSection_Requirement_VerificationStatus_Container']/div[2]/button"))
+					.sendKeys(Keys.TAB, Keys.TAB, "No", Keys.ENTER, Keys.ENTER, Keys.TAB, Keys.TAB);
+			Thread.sleep(2000);
+			driver.findElement(By.xpath("//div[@id='RequirementSection_TopActionPanel']")).click();
+			Thread.sleep(3000);
+			
+			
+			driver.findElement(By.xpath(
+					"//*[@id=\"RequirementListSection_RDP_ThumbnailViewer_CARD\"]/div[4]/div/div[3]/div/div[4]/div[1]/div/ul/li/button[1]/span"))
+					.click();
+			Thread.sleep(3000);
+			driver.findElement(
+					By.xpath("//*[@id='RequirementSection_Requirement_VerificationStatus_Container']/div[2]/button"))
+					.click();
+			Thread.sleep(2000);
+			driver.findElement(By.xpath("//td[contains(text(),'Verified')]")).click();
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//*[@id=\"RequirementSection_Requirement_VerificationStatus\"]")).sendKeys(
+					Keys.TAB, Keys.TAB, "2018", Keys.TAB, "Complete Secondary", Keys.ENTER, Keys.ENTER, Keys.TAB,
+					Keys.TAB);
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//*[@id=\"RequirementSection_Requirement_VerificationStatus\"]")).sendKeys(
+					Keys.TAB, Keys.TAB, "2018", Keys.TAB, "Complete Secondary", Keys.ENTER, Keys.ENTER, Keys.TAB,
+					Keys.TAB);
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//*[@id=\"RequirementSection_Requirement_VerificationStatus\"]")).sendKeys(
+					Keys.TAB, Keys.TAB, Keys.TAB, "Complete Secondary", Keys.ENTER, Keys.ENTER, Keys.TAB, Keys.TAB);
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//*[@id=\"RequirementSection_Requirement_VerificationStatus\"]")).sendKeys(
+					Keys.TAB, Keys.TAB, Keys.TAB, "Complete Secondary", Keys.ENTER, Keys.ENTER, Keys.TAB, Keys.TAB);
+			Thread.sleep(2000);
+			driver.findElement(By.xpath("//div[@id='RequirementSection_TopActionPanel']")).click();
+			Thread.sleep(3000);
+			
+		
+			driver.findElement(By.xpath(
+					"//*[@id=\"RequirementListSection_RDP_ThumbnailViewer_CARD\"]/div[4]/div/div[6]/div/div[4]/div[1]/div/ul/li/button[1]/span"))
+					.click();
+			Thread.sleep(3000);
+			driver.findElement(
+					By.xpath("//*[@id='RequirementSection_Requirement_VerificationStatus_Container']/div[2]/button"))
+					.click();
+			Thread.sleep(2000);
+			driver.findElement(By.xpath("//td[contains(text(),'Verified')]")).click();
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//*[@id=\"RequirementSection_Requirement_VerificationStatus\"]"))
+					.sendKeys(Keys.TAB, Keys.TAB, "Year 8 or below", Keys.ENTER, Keys.ENTER, Keys.TAB, Keys.TAB);
+			Thread.sleep(2000);
+			driver.findElement(By.xpath("//*[@id=\"RequirementSection_Requirement_VerificationStatus\"]"))
+					.sendKeys(Keys.TAB, Keys.TAB, "Year 8 or below", Keys.ENTER, Keys.ENTER, Keys.TAB, Keys.TAB);
+			Thread.sleep(2000);
+			driver.findElement(By.xpath("//*[@id=\"RequirementSection_Requirement_VerificationStatus\"]"))
+					.sendKeys(Keys.TAB, Keys.TAB, Keys.TAB, "2018", Keys.ENTER, Keys.ENTER, Keys.TAB, Keys.TAB);
+			Thread.sleep(2000);
+			driver.findElement(By.xpath("//*[@id=\"RequirementSection_Requirement_VerificationStatus\"]"))
+					.sendKeys(Keys.TAB, Keys.TAB, Keys.TAB, "2018", Keys.ENTER, Keys.ENTER, Keys.TAB, Keys.TAB);
+			Thread.sleep(2000);
+			driver.findElement(By.xpath("//*[@id=\"RequirementSection_Requirement_VerificationStatus\"]")).sendKeys(
+					Keys.TAB, Keys.TAB, Keys.TAB, Keys.TAB, Keys.TAB, Keys.TAB, "Australia", Keys.ENTER, Keys.ENTER,
+					Keys.TAB, Keys.TAB);
+			Thread.sleep(2000);
+			driver.findElement(By.xpath("//*[@id=\"RequirementSection_Requirement_VerificationStatus\"]")).sendKeys(
+					Keys.TAB, Keys.TAB, Keys.TAB, Keys.TAB, Keys.TAB, Keys.TAB, "Australia", Keys.ENTER, Keys.ENTER,
+					Keys.TAB, Keys.TAB);
+			Thread.sleep(2000);
+			
+			
+			break;
+			
+			default:
+				System.out.println("Sorry, no such course. Please double check.");
+			
+
 		}
 
 	}
